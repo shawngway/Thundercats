5045fc5d834c2f958271975c0d77f24180bb661a
Zion Brown <zabs95@gmail.com>
2019-08-28 20:44:29 -0400

    accept

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,28 @@ var search = function() {
    
     event.preventDefault()
 
-    var gameTitle = $("#gameInput").val()
+    var gameTitleInput = $("#gameInput").val()
 
-    console.log(gameTitle)
+    console.log(gameTitleInput)
 
     $("#gameInput").val("")
 
 }
+
+//appends divs to game area
+
+var gameImage = "picture"//cover
+
+var gameTitle = "name"//game name
+
+var gameRating = "rating"//game rating
+
+var gameBox = $("<img>").addClass("gamebox");
+
+
+
+
+
+//ends appends to game area
 
 $(document).on("click", "#search", search) //on click of the sumbit button, calls the search function
