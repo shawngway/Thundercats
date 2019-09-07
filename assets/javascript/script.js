6b5dddc51b41029537272f0c22a0a28bb00020a1
Charles Danner <danner.ced1@gmail.com>
2019-09-07 11:32:08 -0400

    chicken

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,12 +5,22 @@ var genreBeingSearched;
 
 $("#submitButton").on("click", function (e) {
   event.preventDefault();
+
   if (($("#gameInput").val().trim() !== "")) {
-      gameTitleInput = $("#gameInput").val().trim();
+      gameTitleInput = $("#gameInput").val().trim().replace(/\s/g, '-');
       console.log(gameTitleInput)
 
 
-    //ajax call referencing title to get id number to get object
+    // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+    var queryURL = "https://api.rawg.io/api/games/" + gameTitleInput + "/suggested?page_size=5";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function (response) {
+          console.log(response)
+      })
 
 
 
@@ -21,6 +31,10 @@ $("#submitButton").on("click", function (e) {
 
 })
 
+$("#gameSugg").on("click", "li", function(event){
+  
+})
+
 $(".genre-buttons").on("click", "button", function (event){
   console.log("chicken")
   console.log(this)
@@ -28,98 +42,6 @@ $(".genre-buttons").on("click", "button", function (event){
 
   console.log(genreBeingSearched)
 })
-
-//appends divs to game area
-
-var gameImage = "picture"//cover
-
-var gameTitle = "name"//game name
-
-var gameRating = "rating"//game rating
-
-var gameBox = $("<img>").addClass("gamebox");
-
-
-// Example queryURL for Giphy API
-
-// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
-var tool = "test";
-var queryURL = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/";
-
-$.ajax({
-  url: "https://api.rawg.io/api/games/worms-armageddon",
-  method: "GET"
-})
-  // After data comes back from the request
-  .then(function (response) {}
-
-
-//function gameSearch() {
-  //$.ajax({
-    url: queryURL,
-    method: "POST",
-    headers: {
-      "user-key": "d61ece206f9dedf20a9aa373ffa29739"
-    },
-    data: "where rating > 98;"
-  }).then(function (response) {
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "POST",
-    //   headers: {
-    //     "user-key": "d61ece206f9dedf20a9aa373ffa29739"
-    //   },
-    //   data: "fields *; where id = " + response[0].id + ";"
-    // }).then(function (pickles){
-    //   console.log(pickles)
-    // });
-    
-    for (var i = 0; i < response.length; i++) {
-      gameById(response[i].id)
-      .then (function (game) {
-        $("body").append(`<p>${game[0].name}</p>`)
-        console.log(game)
-      })
-    }
-
-    console.log(response);
-    console.log(response[0]);
-  }).fail(function (jqXHR, textStatus) {
-    console.error(textStatus)
-  });
-};
-
-function gameById(id) {
-  return $.ajax({
-    url: queryURL,
-    method: "POST",
-    headers: {
-      "user-key": "d61ece206f9dedf20a9aa373ffa29739"
-    },
-    data: "fields *; where id = " + id + ";"
-  })
-}
-
-$(document).on("click", "button", gameSearch);
-console.log(tool);
-
-    // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/";
-    $.ajax({
-      url: queryURL,
-      method: "POST",
-      headers: {
-        "user-key": "d61ece206f9dedf20a9aa373ffa29739"
-      },
-      data: "fields *; where id = 104945;"
-    }).then(function(response) {
-      console.log(response);
-      var title = response[0].name
-      console.log(title)
-      $("#exampleModalLabel").append(title + " Info")
-    }).fail(function(jqXHR, textStatus) { 
-      console.error(textStatus)
-    });
 
 
 //ends appends to game area
