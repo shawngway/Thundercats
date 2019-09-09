83083f0bdb3fec81dfcb7a0a603ed931068efdd7
Anthony McPherson <aqm28202@gmail.com>
2019-09-09 18:34:58 -0400

    commiting my changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
     if ((gameTitleInput !== "")) { //if gameTitleInput is not null
 
       $("#invalidTitle").css({ "display": "none" }) //error message is not there
+      // $( "#gameSugg" ).empty();
 
       //API
 
@@ -26,12 +27,18 @@ $(document).ready(function () {
 
         for (i = 0; i < response.results.length; i++) {
 
-          console.log(response.results[i].name)
+          var cover = $("<img class='cover'>")
 
+
+          
+          console.log(cover.attr("data-name"))
+          
           var cover = $("<img>")
           var div = $("<div>")
           var li = $("<li>")
-
+          
+          
+          div.attr("data-name", response.results[i].name)
 
           div.attr("class", 'uk-panel active')
 
@@ -41,9 +48,11 @@ $(document).ready(function () {
 
           cover.attr("src", response.results[i].short_screenshots[0].image)
 
+
           $("#gameSugg").append(li)
           $(li).append(div)
           $(div).append(cover)
+
 
           
         }
@@ -73,28 +82,31 @@ $(document).ready(function () {
 
   //appends divs to game area
 
-  var gameImage = "picture"//cover
+  // var gameImage = "picture"//cover
 
-  var gameTitle = "name"//game name
+  // var gameTitle = "name"//game name
 
-  var gameRating = "rating"//game rating
+  // var gameRating = "rating"//game rating
 
-  var gameBox = $("<img>").addClass("gamebox");
+  // var gameBox = $("<img>").addClass("gamebox");
 
 
   // Example queryURL for Giphy API
   // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
   var tool = "test";
   var queryURL = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/";
-
+  
   function gameSearch() {
+
+    console.log($(this).attr("data-name"))
+    var metaName = $(this).attr("data-name")
     $.ajax({
       url: queryURL,
       method: "POST",
       headers: {
         "user-key": "d61ece206f9dedf20a9aa373ffa29739"
       },
-      data: 'search "halo 2"; fields *;'
+      data: 'search ' + ' " '+ metaName + ' " ' + '; fields *;'
       //where rating > 99;fields name, category, cover, platforms, videos; limit 4; 
     }).then(function (response) {
       // $.ajax({
@@ -111,8 +123,8 @@ $(document).ready(function () {
       // }
       gameById(response[0].id)
         .then(function (game) {
-          $("body").append(`<li>${game[0].name}</li>`)
-          $("body").append(`<li>${game[0].url}</li>`)
+          $("#gameInfo").html(`<li>${game[0].name}</li>`)
+          $("#gameInfo").append("<li><a href='" + game[0].url + "'>Game Info</a></li>")
 
           console.log(game)
         })
@@ -136,7 +148,7 @@ $(document).ready(function () {
     })
   }
 
-  $(document).on("click", "button", gameSearch);
+  $(document).on("click", ".uk-panel", gameSearch);
   // console.log(tool);
   // // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
   // var queryURL = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/";
@@ -166,6 +178,12 @@ $(document).ready(function () {
 
 // var gameRating = "rating"//game rating
 
+
 // var gameBox = $("<img>").addClass("gamebox");
+
+// var gameBox = $("<img>").addClass("gamebox")
+
+// var gameBox = $("<img>").addClass("gamebox");
+
 
 });
